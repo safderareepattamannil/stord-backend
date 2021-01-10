26c52c03e366054c18e060ce6f7dc76f28f8ae56
Safder Areepattamannil <safderareepattamannil@gmail.com>
2021-01-09 20:13:43 -0500

    removed node_modules

--- index.js
+++ index.js
@@ -1,39 +1,65 @@
 const express = require("express");
 const multer = require("multer");
-const path = require('path')
-
+const path = require("path");
+const fs = require('fs')
+const cors = require('cors')
 const app = express();
-const port = 8080;
-app.use(express.static(__dirname + '/public'));
+const port = 8342;
 
-// SET STORAGE
+// Serve static files from uploads
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use(cors())
+
+// Multer Setup
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/');    },
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
+    },
 
     // By default, multer removes file extensions so let's add them back
-    filename: function(req, file, cb) {
+    filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
-    }
+    },
 });
-
 var upload = multer({ storage: storage });
 
+8
+// GET ROUTES //
 app.get("/", (req, res) => {
-  res.send("Testing Server");
+    res.send("Testing Server");
 });
 
-app.post("/upload", upload.array("media", 10), (req, res, next) => {
-  const files = req.files;
+app.get("/getfiles", (req, res) => {
+    // TODO: ITERATE OVER UPLOADS FOLDER AND SERVE ALL MEDIA CONTENT URLS
+    const directoryPath = path.join(__dirname, 'uploads');
+    fs.readdir(directoryPath, (err,files)=>{
+      if(err){
+        return console.log("Unable to scan directory: " + err);
+      }
+      const fileNames = [];
 
-  if (!files) {
-    const error = new Error("Please upload a file!");
-    error.httpStatusCode = 400;
-    return next(error);
-  }
-  res.send(files);
+      files.forEach(file =>{
+        fileNames.push(file);
+      })
+
+      res.send(fileNames);
+    })
+  });
+
+
+// POST ROUTES //
+app.post("/upload", upload.array("files"), (req, res) => {
+    const files = req.files;
+    console.log(files);
+
+    if (!files) {
+        const error = new Error("Please upload a file!");
+        error.httpStatusCode = 400;
+    }
+    
+    res.send(files);
 });
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+    console.log(`App is running on port ${port}`);
 });
