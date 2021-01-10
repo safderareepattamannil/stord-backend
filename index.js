49756d42a962d09b3e202fb05c63b5e2f9a70cf7
Safder Areepattamannil <safderareepattamannil@gmail.com>
2021-01-09 21:08:09 -0500

    Created file model for media and able to upload to database.

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const fs = require('fs')
-const cors = require('cors')
-const mongoose = require('mongoose');
-
+const fs = require("fs");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Media = require("./models/media");
 
 // Setup
 const app = express();
 const port = 8342;
 
 // Database Setup
-mongoose.connect(process.env.URI, {useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console,'connection error'));
-db.once('open', ()=>{
-  console.log('Connected to database');
-})
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Connected to database");
+});
 
+console.log(Media);
 
 // Serve static files from uploads
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-app.use(cors())
+app.use(cors());
 
 // Multer Setup
 const storage = multer.diskStorage({
@@ -37,7 +41,7 @@ const storage = multer.diskStorage({
 });
 var upload = multer({ storage: storage });
 
-8
+8;
 // GET ROUTES //
 app.get("/", (req, res) => {
     res.send("Testing Server");
@@ -45,30 +49,58 @@ app.get("/", (req, res) => {
 
 app.get("/getfiles", (req, res) => {
     // TODO: ITERATE OVER UPLOADS FOLDER AND SERVE ALL MEDIA CONTENT URLS
-    const directoryPath = path.join(__dirname, 'uploads');
-    fs.readdir(directoryPath, (err,files)=>{
-      if(err){
-        return console.log("Unable to scan directory: " + err);
-      }
-      const fileNames = [];
+    const directoryPath = path.join(__dirname, "uploads");
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            return console.log("Unable to scan directory: " + err);
+        }
+        const fileNames = [];
 
-      files.forEach(file =>{
-        fileNames.push(file);
-      })
-      res.send(fileNames);
-    })
-  });
-
+        files.forEach((file) => {
+            fileNames.push(file);
+        });
+        res.send(fileNames);
+    });
+});
 
 // POST ROUTES //
 app.post("/upload", upload.array("files"), (req, res) => {
     const files = req.files;
-    console.log(files);
     if (!files) {
         const error = new Error("Please upload a file!");
         error.httpStatusCode = 400;
     }
-    // TODO: Add to MONGODB Data BAse
+
+    // Add and upload to metadata database
+    let fileModels = [];
+
+    files.forEach((file) => {
+        const media = new Media({
+            name: file.filename,
+            dateUploaded: Date.now(),
+            dateTaken: Date.now(),
+            location: [-322, 344],
+            delete: {marked: false,expiryDate: Date.now()},
+            fileSize: file.size,
+        });
+
+        fileModels.push(media);
+    });
+
+    if (fileModels.length) {
+        db.collection("media")
+            .insertMany(fileModels)
+            .then((result) => {
+                console.log(
+                    `Successfully inserted ${files.length} items!`
+                );
+                return result;
+            })
+            .catch((err) =>
+                console.error(`Failed to insert documents: ${err}`)
+            );
+    }
+
     res.send(files);
 });
 
