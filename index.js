947009ca9c7efe70106400f80fe98ed0808843b4
Safder Areepattamannil <safderareepattamannil@gmail.com>
2021-01-10 20:46:11 -0500

    Created placeholder folders

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const path = require("path");
 const fs = require("fs");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const sharp = require("sharp");
+
+// Required Models
 const Media = require("./models/media");
 
 // Setup
@@ -22,8 +25,6 @@ db.once("open", () => {
     console.log("Connected to database");
 });
 
-console.log(Media);
-
 // Serve static files from uploads
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(cors());
@@ -33,7 +34,6 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "uploads/");
     },
-
     // By default, multer removes file extensions so let's add them back
     filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
@@ -41,21 +41,18 @@ const storage = multer.diskStorage({
 });
 var upload = multer({ storage: storage });
 
-8;
-// GET ROUTES //
+// *  GET ROUTES  --------------------------------------------//
 app.get("/", (req, res) => {
     res.send("Testing Server");
 });
 
 app.get("/getfiles", (req, res) => {
-    // TODO: ITERATE OVER UPLOADS FOLDER AND SERVE ALL MEDIA CONTENT URLS
     const directoryPath = path.join(__dirname, "uploads");
     fs.readdir(directoryPath, (err, files) => {
         if (err) {
             return console.log("Unable to scan directory: " + err);
         }
         const fileNames = [];
-
         files.forEach((file) => {
             fileNames.push(file);
         });
@@ -63,7 +60,7 @@ app.get("/getfiles", (req, res) => {
     });
 });
 
-// POST ROUTES //
+// * POST ROUTES  -------------------------------------------- //
 app.post("/upload", upload.array("files"), (req, res) => {
     const files = req.files;
     if (!files) {
@@ -80,20 +77,25 @@ app.post("/upload", upload.array("files"), (req, res) => {
             dateUploaded: Date.now(),
             dateTaken: Date.now(),
             location: [-322, 344],
-            delete: {marked: false,expiryDate: Date.now()},
+            delete: { marked: false, expiryDate: Date.now() },
             fileSize: file.size,
         });
 
         fileModels.push(media);
+
+        // TODO : Process image thumbnail / get this working
+
+        sharp(path.join(__dirname, file.filename))
+            .resize({ width: 200 })
+            .toFormat("jpeg")
+            .toFile(path.join(__dirname, `/previews/resized.jpg`));
     });
 
     if (fileModels.length) {
         db.collection("media")
             .insertMany(fileModels)
             .then((result) => {
-                console.log(
-                    `Successfully inserted ${files.length} items!`
-                );
+                console.log(`Successfully inserted ${files.length} items!`);
                 return result;
             })
             .catch((err) =>
